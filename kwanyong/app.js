0c62671a3642ba4e28ca1210d8b9ecdb1cb65c05
mixer0326@gmail.com <igwan-yong@igwan-yong-ui-MacBookPro.local>
2023-05-16 11:00:30 +0900

    초기환경셋팅

---
+++ kwanyong/app.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const { DataSource } = require('typeorm');
+
+
+const appDataSource = new DataSource({
+    type: process.env.DB_CONNECTION,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE
+})
+
+appDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+
+const app = express()
+ 
+app.use(cors());
+app.use(logger('combined'));
+
+const port = process.env.PORT
+
+app.get('/ping', function (req, res, next) {
+  res.json({message: 'pong'})
+})
+ 
+app.listen(port, function () {
+  console.log('server listening on port 8000')
+})

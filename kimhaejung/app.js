ecdf8109042a886faaa6a12e2a9628815adc63cb
HaeJungg <gpwnd0120@gmail.com>
2023-05-16 15:49:52 +0900

    [김혜중] Assignment 1: Express API - 초기 환경 세팅

--- kimhaejung/app.js
+++ kimhaejung/app.js
@@ -1,20 +1,19 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
-const { DataSource } = require("typeorm");
-const app = express();
 const logger = require("morgan");
 
-dotenv.config();
+const { DataSource } = require("typeorm");
+
+const port = process.env.PORT || 3000;
+
+const app = express();
 
 app.use(cors());
 app.use(logger("combined"));
 
-app.get("/ping", function (req, res, next) {
-  res.json({ message: "pong" });
-});
-
-const myDataSource = new DataSource({
+const appDataSource = new DataSource({
   type: process.env.DB_CONNECTION,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
@@ -23,10 +22,19 @@ const myDataSource = new DataSource({
   database: process.env.DB_DATABASE,
 });
 
-myDataSource.initialize().then(() => {
-  console.log("Data Source has been initialized!");
+appDataSource
+  .initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization", err);
+  });
+
+app.get("/ping", function (req, res, next) {
+  res.json({ message: "pong" });
 });
 
-app.listen(3000, function () {
-  console.log("server listening on port 3000");
+app.listen(port, function () {
+  console.log(`server listening on port ${port}`);
 });
--- kimhaejung/db/migrations/20230515125712_create_comments_table.sql
+++ kimhaejung/db/migrations/20230515125712_create_comments_table.sql
@@ -1,5 +1,15 @@
 -- migrate:up
-
+create table comments (
+  id int not null auto_increment,
+  content varchar(3000) not null,
+  user_id int not null,
+  post_id int not null,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
+	PRIMARY KEY(id),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (post_id) REFERENCES posts(id)
+)
 
 -- migrate:down
-
+DROP TABLE comments;

7bc6e75a7e5e5cd08b660676553a8d9c13fb745b
HaeJungg <gpwnd0120@gmail.com>
2023-05-17 10:56:55 +0900

    들여쓰기 수정

--- kimhaejung/db/migrations/20230515125712_create_comments_table.sql
+++ kimhaejung/db/migrations/20230515125712_create_comments_table.sql
@@ -5,8 +5,8 @@ create table comments (
   user_id int not null,
   post_id int not null,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	updated_at TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
-	PRIMARY KEY(id),
+  updated_at TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY(id),
   CONSTRAINT comments_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id),
   CONSTRAINT comments_post_id_fkey FOREIGN KEY (post_id) REFERENCES posts(id)
 )

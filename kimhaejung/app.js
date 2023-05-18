2a0e9df119090e573091c3b395c378ed0b837239
HaeJungg <gpwnd0120@gmail.com>
2023-05-18 20:42:16 +0900

    Assignment 6, 7, 8

--- kimhaejung/app.js
+++ kimhaejung/app.js
@@ -10,6 +10,10 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
+app.listen(port, function () {
+  console.log(`server listening on port ${port}`);
+});
+
 app.use(cors());
 app.use(logger("combined"));
 app.use(express.json());
@@ -52,6 +56,59 @@ app.post("/users/signup", async (req, res, next) => {
   res.status(201).json({ message: "userCreated" });
 });
 
-app.listen(port, function () {
-  console.log(`server listening on port ${port}`);
+//
+app.patch("/posts/:userId/:postId", async (req, res, next) => {
+  const { userId, postId } = req.params;
+  const { content } = req.body;
+  await appDataSource.query(
+    ` UPDATE 
+    posts 
+    SET content=? 
+    WHERE user_id=${userId} AND id=${postId};
+    `,
+    [content]
+  );
+
+  const updatedPosts = await appDataSource.query(
+    `SELECT
+        users.id AS userId,
+        users.name AS userName,
+        posts.id AS postingId,
+        posts.title AS postingTitle,
+        posts.content AS postingContent
+        FROM users 
+        INNER JOIN posts 
+        ON posts.user_id = users.id
+        WHERE users.id=${userId} AND posts.id=${postId};
+      `
+  );
+
+  console.log(updatedPosts);
+  res.status(200).json({ data: updatedPosts });
+});
+
+app.delete("/posts/delete/:id", async (req, res) => {
+  const { id } = req.params;
+  const result = await appDataSource.query(
+    `DELETE
+    FROM posts
+    WHERE id = ${id}
+    `
+  );
+  console.log(result);
+  res.status(200).json({ message: "PostingDeleted" });
+});
+
+app.post("/likes/:userId/:postId", async (req, res, next) => {
+  const { userId, postId } = req.params;
+
+  const likesCreated = await appDataSource.query(
+    `INSERT INTO likes(
+            user_id, 
+            post_id
+        ) VALUES (${userId}, ${postId});
+        `
+  );
+  console.log(likesCreated);
+  res.status(200).json({ message: "likeCreated" });
 });

8950b63339ffba51112fe6802d45c489607b7ad4
HaeJungg <gpwnd0120@gmail.com>
2023-05-23 16:32:59 +0900

    use Bcrypt

--- kimhaejung/services/userService.js
+++ kimhaejung/services/userService.js
@@ -1,21 +1,20 @@
 const userDao = require("../models/userDao");
 
+const bcrypt = require("bcrypt");
+
 const signUp = async (name, email, password, profileImage) => {
-  const pwValidation = new RegExp(
-    "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,20})"
-  );
-  if (!pwValidation.test(password)) {
-    const err = new Error("PASSWORD_IS_NOT_VALID");
-    err.statusCode = 409;
-    throw err;
-  }
+  const saltRounds = 12;
+  const makeHash = async (password, saltRounds) => {
+    return await bcrypt.hash(password, saltRounds);
+  };
+
+  const hashedPassword = await makeHash(password, saltRounds);
   const createUser = await userDao.createUser(
     name,
     email,
-    password,
+    hashedPassword,
     profileImage
   );
-
   return createUser;
 };
 

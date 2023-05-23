7655ab19baf743ed183e3e27d7904f6a04666a89
HaeJungg <gpwnd0120@gmail.com>
2023-05-23 13:24:10 +0900

    layered pattern

--- kimhaejung/app.js
+++ kimhaejung/app.js
@@ -3,166 +3,39 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const logger = require("morgan");
-
-const { DataSource } = require("typeorm");
-
-const port = process.env.PORT || 3000;
+const http = require("http");
+const routes = require("./routes");
+const { appDataSource } = require("./models/dataSource");
 const app = express();
-
-app.listen(port, function () {
-  console.log(`server listening on port ${port}`);
-});
 
 app.use(cors());
 app.use(logger("combined"));
 app.use(express.json());
+app.use(routes);
 
-const appDataSource = new DataSource({
-  type: process.env.DB_CONNECTION,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-});
-
-appDataSource
-  .initialize()
-  .then(() => {
-    console.log("Data Source has been initialized!");
-  })
-  .catch((err) => {
-    console.error("Error during Data Source initialization", err);
-  });
-
-app.get("/ping", function (req, res, next) {
+app.get("/ping", (req, res) => {
   res.json({ message: "pong" });
 });
 
-app.post("/users/signup", async (req, res, next) => {
-  const { name, email, profileImage, password } = req.body;
+const server = http.createServer(app);
+const PORT = process.env.PORT || 3000;
 
-  await appDataSource.query(
-    `
-    INSERT INTO users(
-      name,
-      email,
-      profile_image,
-      password
-    ) VALUES (?, ?, ?, ?);
-    `,
-    [name, email, profileImage, password]
-  );
-  res.status(201).json({ message: "userCreated" });
-});
+const start = async () => {
+  try {
+    appDataSource
+      .initialize()
+      .then(() => {
+        console.log("Data Source has been initialized!");
+      })
+      .catch((err) => {
+        console.error("Error occurred during Data Source initialization", err);
+        myDataSource.destroy();
+      });
 
-app.post("/posts", async (req, res, next) => {
-  const { title, content, userId, imageUrl } = req.body;
+    server.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-  await appDataSource.query(
-    `
-    INSERT INTO posts(
-      title,
-      content,
-      user_id,
-      image_url
-    ) VALUES (?, ?, ?, ?);
-    `,
-    [title, content, userId, imageUrl]
-  );
-  res.status(201).json({ message: "postCreated" });
-});
-
-app.get("/posts", async (req, res, next) => {
-  const result = await appDataSource.query(`
-    SELECT
-      users.id AS userId,
-      users.profile_image AS userProfileImage,
-      posts.id AS postingId,
-      posts.image_url AS postingImageUrl,
-      posts.content AS postingContent
-    FROM users
-    INNER JOIN posts ON users.id = posts.user_id`);
-
-  console.log(result);
-  return res.status(200).json({ data: result });
-});
-
-app.get("/users/posts/:id", async (req, res, next) => {
-  const { id } = req.params;
-
-  const usersResult = await appDataSource.query(
-    `
-    SELECT
-      users.id AS userId,
-      users.profile_image AS userProfileImage,
-      JSON_ARRAYAGG(
-        JSON_OBJECT(
-            "postingId", posts.id,
-            "postingImageUrl", posts.image_url,
-            "postingContent", posts.content
-            )
-      ) AS postings
-    FROM posts
-    INNER JOIN users ON posts.user_id = users.id
-    WHERE user_id = ?
-    GROUP BY users.id
-    `,
-    [id]
-  );
-  console.log(usersResult);
-  return res.status(200).json({ data: usersResult });
-});
-
-app.patch("/posts/:postId", async (req, res, next) => {
-  const { postId } = req.params;
-  const { content, userId } = req.body;
-  await appDataSource.query(
-    `
-    UPDATE 
-      posts 
-    SET content=? 
-    WHERE user_id=? AND id=?;
-    `,
-    [content, userId, postId]
-  );
-
-  const updatedPosts = await appDataSource.query(`
-    SELECT
-      users.id AS userId,
-      users.name AS userName,
-      posts.id AS postingId,
-      posts.title AS postingTitle,
-      posts.content AS postingContent
-    FROM users 
-    INNER JOIN posts ON posts.user_id = users.id
-    WHERE users.id=${userId} AND posts.id=${postId};
-      `);
-
-  console.log(updatedPosts);
-  res.status(200).json({ data: updatedPosts });
-});
-
-app.delete("/posts/:id", async (req, res) => {
-  const { id } = req.params;
-  const result = await appDataSource.query(`
-    DELETE
-    FROM posts
-    WHERE id = ${id}
-    `);
-  console.log(result);
-  res.status(200).json({ message: "PostingDeleted" });
-});
-
-app.post("/likes/users/:userId/posts/:postId", async (req, res, next) => {
-  const { userId, postId } = req.params;
-
-  const likesCreated = await appDataSource.query(`
-    INSERT INTO likes(
-      user_id, 
-      post_id
-      ) VALUES (${userId}, ${postId});
-        `);
-  console.log(likesCreated);
-  res.status(204).json({ message: "likeCreated" });
-});
+start();
---
+++ kimhaejung/controllers/postController.js
@@ -0,0 +1,66 @@
+const postService = require("../services/postService");
+
+const posting = async (req, res) => {
+  try {
+    const { title, content, userId, imageUrl } = req.body;
+
+    await postService.posting(title, content, userId, imageUrl);
+    return res.status(201).json({
+      message: "POST_CREATED",
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(err.statusCode || 500).json({ message: err.message });
+  }
+};
+
+const getAllPosts = async (req, res) => {
+  try {
+    const posts = await postService.getAllPosts();
+    return res.status(200).json(posts);
+  } catch (err) {
+    console.log(err);
+    return res.status(err.statusCode || 500).json({ message: err.message });
+  }
+};
+
+const getIdPosts = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const userPost = await postService.getIdPosts(userId);
+    return res.status(200).json({ data: userPost });
+  } catch (err) {
+    console.log(err);
+    return res.status(err.statusCode || 500).json({ message: err.message });
+  }
+};
+
+const updatedPost = async (req, res) => {
+  try {
+    const { content, userId, postId } = req.body;
+    await postService.updatedPost(content, userId, postId);
+    return res.status(200).json({ message: "Post Updated" });
+  } catch (err) {
+    console.log(err);
+    return res.status(err.statusCode || 500).json({ message: err.message });
+  }
+};
+
+const deletedPost = async (req, res) => {
+  try {
+    const { userId, postId } = req.body;
+    await postService.deletedPost(userId, postId);
+    return res.status(204).json({ message: "Posting Deleted" });
+  } catch (err) {
+    console.log(err);
+    return res.status(err.statusCode || 500).json({ message: err.message });
+  }
+};
+
+module.exports = {
+  posting,
+  getAllPosts,
+  getIdPosts,
+  updatedPost,
+  deletedPost,
+};
